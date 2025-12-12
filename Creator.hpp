28a490316eda18e74f3bdbc2cbe2951a973e9655
Oleksandr <oleksandr@frkd.dev>
2025-12-12 12:03:26 +0100

    Initial version of Creator

---
+++ Creator.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+/**
+ * \brief Creator is an extremely simple, but helpful dependency injection mechanism implementing a Dependency Inversion principle from SOLID based on couple of templates and two just methods.
+ */
+
+#include <functional>
+#include <memory>
+
+#include "CreatorVariants.hpp"
+
+template <class ReturnType, Variant, class... Args>
+using Creator = std::function<ReturnType(Args...)>;
+
+template <class ReturnType, Variant Var, class... Args>
+Creator<ReturnType, Var, Args...>& getCreator();
+
+template <class ReturnType, Variant Var, class... Args, class ManagedReturnType = std::unique_ptr<ReturnType>>
+ManagedReturnType
+createUnique(Args... args)
+{
+    auto const& creator = getCreator<ManagedReturnType, Var, Args...>();
+    return creator(args...);
+}
+
+template <class ReturnType, Variant Var, class... Args, class ManagedReturnType = std::shared_ptr<ReturnType>>
+ManagedReturnType
+createShared(Args... args)
+{
+    auto const& creator = getCreator<ManagedReturnType, Var, Args...>();
+    return creator(args...);
+}
